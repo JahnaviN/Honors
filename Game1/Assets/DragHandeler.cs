35bd97f25395a9a071c15b2beb1c362a51cfbc94
Jahnavi Nukireddy <nukireddy.jahnavi@students.iiit.ac.in>
2016-03-10 02:40:51 +0530

    Level1

---
+++ Game1/Assets/DragHandeler.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using System;
+using UnityEngine.UI;
+
+public class DragHandeler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    public static GameObject itemBeingDragged;
+    Vector3 startPosition;
+    Transform startParent;
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        itemBeingDragged = gameObject;
+        startPosition = transform.position;
+        startParent = transform.parent;
+        //Debug.Log(startParent);
+        GetComponent<CanvasGroup>().blocksRaycasts = false;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = Input.mousePosition;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        //Debug.Log(transform.parent);
+        itemBeingDragged = null;
+        GetComponent<CanvasGroup>().blocksRaycasts = true;
+        if (transform.parent == startParent)
+        {
+            transform.position = startPosition;
+            //Debug.Log("entered");
+        }
+    }
+}
---
+++ Game1/Assets/SlotHandeler.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using System;
+using System.Collections.Generic;
+
+public class SlotHandeler : MonoBehaviour, IDropHandler {
+
+    public Dictionary<string, int> dict;
+
+	public GameObject item
+    {
+        get
+        {
+            if(transform.childCount > 0)
+            {
+                return transform.GetChild(0).gameObject;
+            }
+            return null;
+        }
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        if(!item)
+        {
+            DragHandeler.itemBeingDragged.transform.SetParent(transform);
+        }
+    }
+}
