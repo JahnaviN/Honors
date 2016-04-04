c7c3b6c8d23d6b1809acd729257abb5e0bc7ab6d
Uppara Nithiya Shree <unithiyashree@gmail.com>
2016-04-04 23:51:31 +0530

    added backgrounds to levels

---
+++ Game1/Assets/images/Level3/L3_q5/gridding.m
@@ -0,0 +1,46 @@
+img = imread('img.jpg');  %# Load a sample 3-D RGB image
+
+%img(120:120:end,:,:) = 0;       
+%img(:,120:120:end,:) = 0;
+
+% size of option = 150 x 150
+% a = img(100:220,200:320,:);
+
+w = 120;
+
+x = 850;
+y = 150;
+a = img(x:x+w,y:y+w,:);
+imwrite(a,'p1.jpg');
+img(x:x+w,y:y+w,:) = 255;
+
+x = 300;
+y = 100;
+a = img(x:x+w,y:y+w,:);
+imwrite(a,'p2.jpg');
+img(x:x+w,y:y+w,:) = 255;
+
+x = 40;
+y = 1500;
+a = img(x:x+w,y:y+w,:);
+imwrite(a,'p3.jpg');
+img(x:x+w,y:y+w,:) = 255;
+
+x = 700;
+y = 900;
+a = img(x:x+w,y:y+w,:);
+imwrite(a,'p4.jpg');
+img(x:x+w,y:y+w,:) = 255;
+
+x = 400;
+y = 1550;
+a = img(x:x+w,y:y+w,:);
+imwrite(a,'p5.jpg');
+img(x:x+w,y:y+w,:) = 255;
+
+imshow(img);
+
+imshow(img)
+imwrite(img,'n.jpg');
+
+
